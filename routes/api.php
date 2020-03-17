b547fa560eb1f02cbdfe1642545825005cacba39
unknown <devneonix@gmail.com>
2020-03-16 20:54:55 -0500

    20

--- routes/api.php
+++ routes/api.php
@@ -41,7 +41,7 @@ Route::get('ots_personal', function (Request $request) {
 Route::get('personal_ots', function (Request $request) {
     $dni = $request->input("dni");
 
-    if (empty( DB::table("PERSONAL")->where('doc_ide','=',$dni)->get())){
+    if (empty( DB::table("personal")->where('doc_ide','=',$dni)->get())){
         return response()->json(apiResponse([],"Upss no tenemos a este personal registrado"), 200, [], 256);
     }
     $exec = DB::table("view_orden_trabajo_personal")->where('doc_ide','=',$dni)->get();
